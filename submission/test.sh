afcecb5871383e0ac4078a8597873f11b69c727d
sawe-daisy <deesawe16@gmail.com>
2025-03-19 17:52:48 +0300

    bitcoin-cli commands

--- submission/test.sh
+++ submission/test.sh
@@ -176,7 +176,7 @@ check_cmd "Getting address info"
 
 # STUDENT TASK: Extract the internal key (the x-only pubkey) from the descriptor
 # WRITE YOUR SOLUTION BELOW:
-INTERNAL_KEY=$(echo "$ADDR_INFO" | jq -r '.pubkey')
+INTERNAL_KEY=$(echo "$ADDR_INFO" | jq -r '.tap_internal_key')
 check_cmd "Extracting key from descriptor"
 INTERNAL_KEY=$(trim "$INTERNAL_KEY")
 
